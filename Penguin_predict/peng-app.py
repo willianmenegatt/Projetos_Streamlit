75a1bbb38e6ae9e8f2671939334205e127174acc
Willian Favero Menegatt <92998955+willianmenegatt@users.noreply.github.com>
2022-04-06 20:13:26 -0300

    Add files via upload

---
+++ Penguin_predict/peng-app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Penguin Prediction App
+
+This app predicts the **Palmer Penguin** species!
+
+**Data source**: https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst
+""")
+
+st.sidebar.header('User Input Features')
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+""")
+
+# In this web app, there are 2 possibilities to input de features
+    # 1. The user upload the file with the info
+    # 2. The user choose in slide-bars the info
+
+# Collects user input features into dataframe
+uploaded_file = st.sidebar.file_uploader('Upload yout input CSV file', type=['csv'])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        island = st.sidebar.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+        sex = st.sidebar.selectbox('Sex', ('male','female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+        data = {'island': island,
+                'bill_length_mm': bill_length_mm,
+                'bill_depth_mm': bill_depth_mm,
+                'flipper_length_mm': flipper_length_mm,
+                'body_mass_g': body_mass_g,
+                'sex': sex}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+
+# Combines use input features with entire penguins dataset
+# This will be useful for the encoding phase - analyse the various types of qualitative columns instead of 1 (input argument)
+penguins_raw = pd.read_csv('C:/Users/Usuario/Desktop/Data_Science/STREAMLIT_PROJECTS/CLASSIFICATION_PENGUINS/penguins_cleaned.csv')
+penguins = penguins_raw.drop('species', axis=1).copy()
+df = pd.concat([input_df, penguins], axis=0)
+
+# Encoding of ordinal features
+encode = ['sex', 'island']
+df = pd.get_dummies(data=df, columns=encode)
+df = df[:1]
+
+st.subheader('User input features')
+
+if uploaded_file is not None:
+    st.write(df)
+else:
+    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+    st.write(df)
+
+# Read in saved classification model
+load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+
+# Apply model to make predictions
+prediction = load_clf.predict(df)
+prediction_proba = load_clf.predict_proba(df)
+
+st.subheader('Prediction')
+penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+st.write(penguins_species[prediction])
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
+
+
+
+
+
+    
+
+
+
+
+
+
+
+# Paste in Terminal: streamlit run c:/Users/Usuario/Desktop/Data_Science/STREAMLIT_PROJECTS/CLASSIFICATION_PENGUINS/peng-app.py [ARGUMENTS]
